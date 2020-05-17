14d25f240daaff78870f22c72b0db1d77fa47a65
Evgeny Kochetkov <evgeny.kochetkov@me.com>
2020-05-17 19:32:01 +0300

    feat(stdlib): add xod/waves library
    and deprecate *-wave nodes from xod/core:
    xod/core/saw-wave
    xod/core/saw-wave-map
    xod/core/sine-wave
    xod/core/sine-wave-map
    xod/core/square-wave
    xod/core/tri-wave
    xod/core/tri-wave-map

---
+++ workspace/__lib__/xod/waves/dividable-timer/patch.cpp
@@ -0,0 +1,26 @@
+
+struct State {
+    TimeMs lastUpdateTime;
+};
+
+{{ GENERATED_CODE }}
+
+void evaluate(Context ctx) {
+    auto state = getState(ctx);
+    TimeMs t = transactionTime();
+    Number divider = getValue<input_DIV>(ctx);
+
+    if (divider <= 0) {
+        raiseError(ctx);
+    }
+    else if (isInputDirty<input_RST>(ctx)) {
+        emitValue<output_OUT>(ctx, 0);
+    }
+    else if (getValue<input_EN>(ctx) && isInputDirty<input_UPD>(ctx)) {
+        Number currentOutput = getValue<output_OUT>(ctx);
+        Number dtSeconds = (t - state->lastUpdateTime) / 1000.0;
+        emitValue<output_OUT>(ctx, currentOutput + (dtSeconds / divider));
+    }
+
+    state->lastUpdateTime = t;
+}
